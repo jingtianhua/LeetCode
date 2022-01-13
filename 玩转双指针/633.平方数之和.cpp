b1a786928d6e3189bfd33df757ed6eaf38fe30d0
jingtianhua <1264585923@qq.com>
2022-01-13 15:59:28 +0800

    ac

--- 玩转双指针/633.平方数之和.cpp
+++ 玩转双指针/633.平方数之和.cpp
@@ -3,12 +3,24 @@
  *
  * [633] 平方数之和
  */
-
+#include <math.h>
 // @lc code=start
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-
+        //缩短问题规模，时间复杂度位O(根号n)
+        long low = 0, high = floor(sqrt(c) + 0.5);
+        while (low <= high) {
+            long sum = low * low + high * high;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return false;
     }
 };
 // @lc code=end
